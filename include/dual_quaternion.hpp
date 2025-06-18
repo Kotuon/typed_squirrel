b5cba1ecfd21f0b51c4d8a47f4b0acd15f2050ca
Kelson Wysocki <32180124+Kotuon@users.noreply.github.com>
2025-06-18 14:59:09 -0700

    Added conversion to matrix and functions to get translation and rotation portions of dual quaternions.

--- include/dual_quaternion.hpp
+++ include/dual_quaternion.hpp
@@ -107,6 +107,48 @@ public:
         return DualQuaternion( real.conjugate(), -dual.conjugate() );
     }
 
+    constexpr const Quaternion& getRotation() const { return real; }
+
+    constexpr const vector3 getTranslation() const {
+        Quaternion t = ( dual * 2.f ) * real.conjugate();
+        return vector3( t.i, t.j, t.k );
+    }
+
+    constexpr const matrix4 getMatrix() {
+        normalize();
+
+        const float& w = real.w;
+        const float& i = real.i;
+        const float& j = real.j;
+        const float& k = real.k;
+
+        const float w2 = real.w * real.w;
+        const float i2 = real.i * real.i;
+        const float j2 = real.j * real.j;
+        const float k2 = real.k * real.k;
+
+        const vector3 t = getTranslation();
+
+        matrix4 result = { w2 + i2 - j2 - k2,
+                           2.f * i * j - 2.f * w * k,
+                           2.f * i * k + 2.f * w * j,
+                           t.x,
+                           2.f * i * j + 2.f * w * k,
+                           w2 - i2 + j2 - k2,
+                           2.f * j * k - 2.f * w * i,
+                           t.y,
+                           2.f * i * k - 2.f * w * j,
+                           2.f * j * k + 2.f * w * i,
+                           w2 - i2 - j2 + k2,
+                           t.z,
+                           0.f,
+                           0.f,
+                           0.f,
+                           1.f };
+
+        return result;
+    }
+
 public:
     Quaternion real;
     Quaternion dual;
